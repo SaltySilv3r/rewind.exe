0415e94603b485df8f88f4e4353fc62469e0182e
Luna <luna.rachmani@student.nhlstenden.com>
2025-03-24 21:33:07 +0100

    i am a dummy and had my URL constants doubled :D

--- site.js
+++ site.js
@@ -1,12 +1,3 @@
-// URL constants for Rewind.exe
-export const battlefieldCenterURL = "https://www.battlefieldcenterz.com";
-export const chaosTheoryForumsURL = "https://www.chaostheoryforums.com";
-export const chaosTheoryConspiracyURL = "https://www.chaostheoryforums.com/conspiracy";
-export const huntTheWitchURL = "https://www.huntthewitch.com";
-export const huntTheWitchInvestigationURL = "https://www.huntthewitch.com/investigation";
-export const finalChallengeURL = "https://www.final-challenge.com/1213";
-export const underConstructionURL = "under-construction";
-
 // Game constants
 export const GAME_ENTRY_CODE = "0710";
 export const GAME_ID = "rewindExeGame";
